d88c97232f6515dd0f4607ffb0f51f5472002af5
unknown <andrxsmrtnzz@gmail.com>
2025-12-23 20:07:29 +0100

    Subo proyecto TDS1 inicial

---
+++ TDS1/src/prueba/PruebaUsuario.java
@@ -0,0 +1,8 @@
+package prueba;
+import wow.Usuario;
+public class PruebaUsuario {
+public static void main(String[] args) {
+	Usuario u=new Usuario("Rafa");
+	System.out.println(u.toString());
+}
+}
---
+++ TDS1/src/wow/Usuario.java
@@ -0,0 +1,13 @@
+package wow;
+
+public class Usuario {
+	String nombre;
+public	Usuario (String nombre) {
+	this.nombre=nombre;
+}
+@Override
+public String toString() {
+	return "Usuario [nombre=" + nombre + "]";
+}
+
+}
