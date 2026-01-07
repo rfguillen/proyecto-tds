b74d8e7cf83b3ad6ab1448d953c09184070179f1
Rafael <147556227+rfguillen@users.noreply.github.com>
2026-01-07 23:07:10 +0100

    Delete TDS1 directory
    esto es del principio

--- TDS1/src/prueba/PruebaUsuario.java
+++
@@ -1,8 +0,0 @@
-package prueba;
-import wow.Usuario;
-public class PruebaUsuario {
-public static void main(String[] args) {
-	Usuario u=new Usuario("Rafa");
-	System.out.println(u.toString());
-}
-}
